50bc2acb7d3d949c213e7cc2a1d4999cd6407019
Jackson Ivan <zlystsistem@gmail.com>
2022-07-01 11:25:52 -0300

    Perfil Discord

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function projetos() {
+  const project = document.getElementById("projetos")
+  project.style.display = "block"
+}
+function exitProjetos() {
+  const project = document.getElementById("projetos")
+  project.style.display = "none"
+}
